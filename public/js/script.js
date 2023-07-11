0f4ea21005b8a189bf9a1edc844f035f3763b0a7
zone <43332748+plateau11@users.noreply.github.com>
2023-07-11 06:17:04 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -34,7 +34,7 @@ weatherForm.addEventListener('submit',(e)=>{
 
         msg1.textContent = 'Loading...'
 
-        fetch('http://weather-site-kuln.onrender.com/weather?address='+search.value+"'").then((response)=>{
+        fetch('http://localhost:10000/weather?address='+search.value+"'").then((response)=>{
     response.json().then((data)=>{
         if(!data.error && !data.error2){
            // console.log(data.temperature)
